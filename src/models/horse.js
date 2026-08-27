feda802e2854e65fa1df62f2fedec4f3194b3e41
arseni_novik <novikarsen@gmail.com>
2026-08-27 13:09:18 +0200

    Update horse.js

--- src/models/horse.js
+++ src/models/horse.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const Country = require('./country');
-const Breeder = require('./breeder');
 
 const horseSchema = new mongoose.Schema(
   {
@@ -51,32 +49,7 @@ const horseSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-horseSchema.pre('validate', async function validateReferences() {
-  const [countryExists, breederExists, motherExists, fatherExists] = await Promise.all([
-    this.countryOfBirth ? Country.exists({ _id: this.countryOfBirth }) : true,
-    this.breeder ? Breeder.exists({ _id: this.breeder }) : true,
-    this.mother ? this.constructor.exists({ _id: this.mother }) : true,
-    this.father ? this.constructor.exists({ _id: this.father }) : true,
-  ]);
-
-  if (!countryExists) {
-    this.invalidate('countryOfBirth', 'Country of birth does not exist.');
-  }
-
-  if (!breederExists) {
-    this.invalidate('breeder', 'Breeder does not exist.');
-  }
-
-  if (!motherExists) {
-    this.invalidate('mother', 'Mother does not exist.');
-  }
-
-  if (!fatherExists) {
-    this.invalidate('father', 'Father does not exist.');
-  }
-});
-
-// A horse is uniquely identified by its name, country of birth, and birth year.
+// A horse is uniquely identified by its name, country of birth, and birth year
 horseSchema.index({ name: 1, countryOfBirth: 1, birthYear: 1 }, { unique: true });
 
 module.exports = mongoose.model('Horse', horseSchema);
